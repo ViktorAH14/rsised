41230b5d906184214f1209a88ea562c64f219156
ViktorAH14 <ermolovva@gmail.com>
2021-06-10 18:52:28 +0300

    На ветке master  Изменения, которые будут включены в коммит: 	изменено:      src/rsised/main.cpp Корректировка main.cpp.

--- src/rsised/main.cpp
+++ src/rsised/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication rsised(argc, argv);
+    MainWindow mainwindow;
+    mainwindow.show();
+    return rsised.exec();
 }
