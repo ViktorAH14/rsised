a316015a615ad8b49a89a8732f823db49cf1cfb3
ViktorAH14 <ermolovva@gmail.com>
2021-06-11 19:24:01 +0300

    На ветке master  Изменения, которые будут включены в коммит: 	изменено:      src/rsised/mainwindow.cpp  Добавил Antialiasing.

--- src/rsised/mainwindow.cpp
+++ src/rsised/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPainter>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -8,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     scene = new DiagramScene(this);
     ui->mainGraphicsView->setScene(scene);
+    ui->mainGraphicsView->setRenderHints(QPainter::Antialiasing);
 }
 
 MainWindow::~MainWindow()
