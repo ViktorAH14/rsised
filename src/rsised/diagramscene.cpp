aabccb9184dbe5915725f90056d0c9080896f2f6
ViktorAH14 <ermolovva@gmail.com>
2021-06-12 16:48:55 +0300

    На ветке develop  Изменения, которые будут включены в коммит: 	изменено:      src/rsised/diagramscene.cpp
     Неотслеживаемые файлы:
    	src/rsised/shapes.cpp
    	src/rsised/shapes.h
     Изменил цвет линии.

--- src/rsised/diagramscene.cpp
+++ src/rsised/diagramscene.cpp
@@ -11,7 +11,7 @@ DiagramScene::DiagramScene(QObject *parent) : QGraphicsScene(parent)
 void DiagramScene::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
 {
     line = new QGraphicsLineItem(QLineF(mouseEvent->scenePos(), mouseEvent->scenePos()));
-    line->setPen(QPen(Qt::black));
+    line->setPen(QPen(Qt::green));
     addItem(line);
 }
 
