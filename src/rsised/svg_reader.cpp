4d8e87a2da69df706682251fb2bc6c4f308a4bdc
ViktorAH14 <ermolovva@gmail.com>
2022-02-06 16:17:52 +0300

    Добавил лицензию во все файлы.

--- src/rsised/svg_reader.cpp
+++ src/rsised/svg_reader.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2022 by Viktor Ermolov <ermolovva@gmail.com>.
+ *
+ * This file is part of the RSiSed project, a editor of the alignment of forces
+ * and means in extinguishing a fire. (RSiSed)
+ *
+ * RSiSed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RSiSed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "svg_reader.h"
 #include "rectangle.h"
 #include "ellipse.h"
