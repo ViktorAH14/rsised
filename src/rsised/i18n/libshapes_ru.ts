aacc577970693213fda12a63d46fe007559fe9c9
ViktorAH14 <ermolovva@gmail.com>
2022-12-08 09:27:15 +0300

    Добавил OpenShape в toolBox.

--- src/rsised/i18n/libshapes_ru.ts
+++ src/rsised/i18n/libshapes_ru.ts
@@ -4,35 +4,35 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../libshapes/buildingshape.cpp" line="552"/>
+        <location filename="../../libshapes/buildingshape.cpp" line="553"/>
         <source>Leaf change</source>
         <translatorcomment>Изменить положение полотна двери</translatorcomment>
         <translation>Изменить полотно</translation>
     </message>
     <message>
-        <location filename="../../libshapes/buildingshape.cpp" line="553"/>
+        <location filename="../../libshapes/buildingshape.cpp" line="554"/>
         <source>Changing the position of the door leaf</source>
         <translation>Изменить позицию дверного полотна</translation>
     </message>
     <message>
-        <location filename="../../libshapes/buildingshape.cpp" line="559"/>
+        <location filename="../../libshapes/buildingshape.cpp" line="560"/>
         <source>Open door</source>
         <translation>Открыть дверь</translation>
     </message>
     <message>
-        <location filename="../../libshapes/buildingshape.cpp" line="560"/>
-        <location filename="../../libshapes/buildingshape.cpp" line="566"/>
-        <location filename="../../libshapes/buildingshape.cpp" line="572"/>
+        <location filename="../../libshapes/buildingshape.cpp" line="561"/>
+        <location filename="../../libshapes/buildingshape.cpp" line="567"/>
+        <location filename="../../libshapes/buildingshape.cpp" line="573"/>
         <source>Change the state of the door</source>
         <translation>Изменить положение дверного полотна</translation>
     </message>
     <message>
-        <location filename="../../libshapes/buildingshape.cpp" line="565"/>
+        <location filename="../../libshapes/buildingshape.cpp" line="566"/>
         <source>Ajar door</source>
         <translation>Прикрыть дверь</translation>
     </message>
     <message>
-        <location filename="../../libshapes/buildingshape.cpp" line="571"/>
+        <location filename="../../libshapes/buildingshape.cpp" line="572"/>
         <source>Close door</source>
         <translation>Закрыть дверь</translation>
     </message>
--- src/rsised/mainwindow.cpp
+++ src/rsised/mainwindow.cpp
@@ -851,8 +851,8 @@ void MainWindow::createShapeToolBox()
                                                                 , BuildingShape::Window), 0, 1);
     p_buildShapeLayout->addWidget(createBuildingShapeCellWidget(tr("Door")
                                                                 , BuildingShape::Door), 0, 2);
-//    buildShapeLayout->addWidget(createBuildingShapeCellWidget(tr("Opening")
-//                                                                , BuildingShape::Open), 1, 0);
+    p_buildShapeLayout->addWidget(createBuildingShapeCellWidget(tr("Opening")
+                                                                , BuildingShape::Open), 1, 0);
     p_buildShapeLayout->setRowStretch(3, 10);
     p_buildShapeLayout->setColumnStretch(3, 10);
     QWidget *p_buildingWidget = new QWidget(this);
