32e9af6295d286d3d77fe02cd77f5a8c4399108a
ViktorAH14 <ermolovva@gmail.com>
2025-01-24 12:31:17 +0300

    feature(libshapes): Add the type() function
    Add the type() function to the EquipmqntShape class.

--- src/include/equipmentshape.h
+++ src/include/equipmentshape.h
@@ -142,6 +142,20 @@ public:
 
     static EquipmentShape *createEquipmentShape(ShapeType shapeType, QGraphicsItem *parent = nullptr);
 
+    /*!
+     * This type information is used by qgraphicsitem_cast() to distinguish
+     * between types.
+     * All standard graphicsitem classes are associated with a unique value;
+     * see QGraphicsItem::Type. The default implementation (in QGraphicsItem)
+     * returns UserType. To enable use of qgraphicsitem_cast() with a custom
+     * item, reimplement this function and declare a Type enum value equal to
+     * your custom item's type. Custom items must return a value larger than
+     * or equal to UserType (65536).
+     *
+     * \return Returns the type of an item as an int.
+     */
+    inline int type() const override {return Type;}
+
     // QRectF boundingRect() const override;
     // int type() const override {return Type;}
 
